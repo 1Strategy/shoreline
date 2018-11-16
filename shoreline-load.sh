b3db5fa833be87b6952dabd34813a046e3105323
Alex <alex@1strategy.com>
2018-11-15 22:43:50 -0700

    add instructions for starting gremlin console

--- shoreline-load.sh
+++ shoreline-load.sh
@@ -1,7 +1,7 @@
 curl -X POST -H 'Content-Type: application/json' \
     http://shoreline.c45ikshrooy3.us-west-2.neptune.amazonaws.com:8182/loader -d '
     { 
-      "source" : "s3://1s-shoreline/", 
+      "source" : "s3://1s-shoreline/edges", 
       "format" : "csv",  
       "iamRoleArn" : "arn:aws:iam::281782457076:role/shoreline", 
       "region" : "us-west-2", 
@@ -9,3 +9,22 @@ curl -X POST -H 'Content-Type: application/json' \
     }'
 
 # curl -G 'http://your-neptune-endpoint:8182/loader/ef478d76-d9da-4d94-8ff1-08d9d4863aa5'
+
+# Set up Gremlin Console
+# sudo yum install java-1.8.0-devel -y
+# sudo /usr/sbin/alternatives --config java
+# wget https://archive.apache.org/dist/tinkerpop/3.3.2/apache-tinkerpop-gremlin-console-3.3.2-bin.zip
+# unzip apache-tinkerpop-gremlin-console-3.3.2-bin.zip
+# cd apache-tinkerpop-gremlin-console-3.3.2
+# cd conf
+# touch neptune-remote.yaml
+#
+# hosts: [shoreline.c45ikshrooy3.us-west-2.neptune.amazonaws.com]
+# port: 8182
+# serializer: { className: org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV3d0, config: { serializeResultToString: true }}
+#
+# cd ../
+# bin/gremlin.sh
+# :remote connect tinkerpop.server conf/neptune-remote.yaml
+# :remote console
+# :exit
