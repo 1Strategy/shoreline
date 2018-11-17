96d68a77bcf459bb8e2f10b0df4a22468d756492
Alex <alex@1strategy.com>
2018-11-16 23:38:40 -0700

    create simpler ~id field values and add weighting

--- shoreline-edges.sql
+++ shoreline-edges.sql
@@ -1,13 +1,14 @@
--- There are NO Property columns created with this query
-
-select distinct md5(to_utf8(split(useridentity.principalId,':')[2] || eventSource)) as "~id"
-     , md5(to_utf8(split(useridentity.principalId,':')[2])) as "~from"
-     , md5(to_utf8(eventSource)) as "~to"
-     , eventName as "~label"
+select 'e' || cast(row_number() over() as varchar) as "~id"
+     , split(useridentity.principalId,':')[2] as "~from"
+     , split(eventSource,'.')[1] as "~to"
+     , eventType as "~label"
+     , count(eventName)/100.0 as "Weight:Double"
   from aws_logs.cloudtrail_partitioned
  where region = 'us-west-2'
    and year = '2018'
-   and resources is not null
    and useridentity.sessioncontext.sessionIssuer.userName = '1S-Admins'
    and not regexp_like(useridentity.principalId, 'botocore')
+   group by useridentity.principalId
+          , eventSource
+          , eventType
 ;
